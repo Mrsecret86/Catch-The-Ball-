598d8111f49ae15344ac58ee756b454c901efe30
Mrsecret86 <150791261+Mrsecret86@users.noreply.github.com>
2023-12-05 17:51:14 +0700

    Meeting 21 Check

--- src/CatchTheBallScene.js
+++ src/CatchTheBallScene.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser'
+import FallingObject from './ui/FallingObject'
 
 export default class CatchTheBallScene extends Phaser.Scene {
 	constructor() {
@@ -7,7 +8,17 @@ export default class CatchTheBallScene extends Phaser.Scene {
 
 	// INIT METHOD
 	init(){
-
+		this.bucket = undefined
+		this.speed = 100
+		this.nav_left = false;
+		this.nav_right = false;
+		this.cursor = undefined
+		this.balls = undefined;
+		this.ballSpeed = 50;
+		this.caughtText = undefined
+		this.caught = 0
+		this.bomb = undefined;
+		this.bombSpeed = 70;
 	}
 
 	// PRELOAD METHOD
@@ -22,12 +33,102 @@ export default class CatchTheBallScene extends Phaser.Scene {
 	// CREATE METHOD
 	create(){
 		this.add.image(306, 216.5, 'background')
-		this.add.image(1, 1, 'bucket')
-		
+		this.bucket = this.createBucket()
+		this.cursor = this.input.keyboard.createCursorKeys()
+		this.balls = this.physics.add.group({
+			classType: FallingObject,
+			maxSize: 10,
+			runChildUpdate: true
+		})
+		this.time.addEvent({
+			delay: Phaser.Math.Between(1000, 5000),
+			callback: this.spawnBalls,
+			callbackScope: this,
+			loop: true
+		})
+		this.caughtText = this.add.text(16, 16, 'Caught : 0', {
+			// @ts-ignore
+			fontSize: '16px', fill: 'black'
+		})
+		this.physics.add.overlap(
+			this.bucket,
+			this.balls,
+			this.caughtBalls,
+			null,
+			this
+		)
 	}
 
 	// UPDATE METHOD
 	update(){
-
+		if (this.cursor.left.isDown){
+			this.bucket.setVelocity(-200, 200)
+		} else if (this.cursor.right.isDown){
+			this.bucket.setVelocity(200, 200)
+		}
+		this.caughtText.setText('Caught: '+ this.caught)
 	}
+
+ 	// CREATE BUCKET METHOD
+	createBucket(){
+		const bucket = this.physics.add.sprite(306, 100, 'bucket').setScale(0.4)
+		.refreshBody();
+		bucket.setCollideWorldBounds(true)
+		return bucket
+	}
+
+	// MOVE BUCKET METHOD
+	moveBucket(bucket, time){
+		if (this.nav_left){
+			this.bucket.setVelocityX(this.speed * -1)
+			this.bucket.setFlipX(false)
+		} else if (this.nav_right){
+			this.bucket.setVelocityX(this.speed)
+			this.bucket.setFlipX(true)
+		} else {
+			this.bucket.setVelocityX(0)
+		}
+	}
+
+	// SPAWN BALLS
+	spawnBalls(){
+		const config = {
+			speed: 30,
+			rotation: 0.1
+		}
+		// @ts-ignore
+		const balls = this.balls.get(0,0, 'ball', config).setScale(0.2)
+		.refreshBody()
+		const positionX = Phaser.Math.Between(50, 350)
+		if (balls) {
+			balls.spawn(positionX)
+		}
+	}
+
+	// CAUGHT BALLS
+	caughtBalls(bucket, balls){
+		balls.die()
+		this.caught += 1
+	}
+
+	// SPAWN Bomb
+		spawnBomb(){
+		const config = {
+			speed: 30,
+			rotation: 0.1
+		}
+		// @ts-ignore
+		const bomb = this.bomb.get(0,0, 'bomb', config).setScale(0.2)
+		.refreshBody()
+		const positionX = Phaser.Math.Between(50, 350)
+		if (bomb) {
+			bomb.spawn(positionX)
+		}
+	}
+
+		// CAUGHT BOMB
+		caughtBomb(bucket, bomb){
+			bomb.die()
+			this.
+		}
 }
